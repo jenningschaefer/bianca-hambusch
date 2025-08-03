bbf520935c820ef998cb7e60babd85c4f37a6542
jenningschaefer <jenningschaefer@googlemail.com>
2025-08-03 17:14:58 +0200

    hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
 	<main id="main" class="home">
-		<MDC :value="home.content" />
+		<!-- <NuxtPicture format="avif, webp" src="/images/bianca.jpg" class="hero" /> -->
+		<img  class="hero"/>
+		<div class="hero-overlay">
+			<img src="/images/logos/logo-hero.svg" alt="logo" class="logo" />
+			<div class="hero-overlay-actions">
+				<NuxtLink to="/"><Button>Blog</Button></NuxtLink>
+				<NuxtLink to="/"><Button>Kontakt</Button></NuxtLink>
+			</div>
+		</div>
 	</main>
 
 </template>
@@ -25,5 +33,40 @@ main {
 	}
 	// assets/scss/mixins
 	@include fade-in;
+}
+
+.hero {
+	position: relative;
+	width: 100%;
+	height: 100vh;
+	//object-fit: cover;
+	//object-position: center;
+	background-image: url("/images/bianca.jpg");
+	background-size: cover;	
+	background-position: center;
+	background-repeat: no-repeat;
+	img {
+		max-width: 100%;
+		height: 100% !important;
+		object-fit: cover;
+		object-position: center;
+	}
+	&-overlay {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: $spacing3;
+		&-actions {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			gap: $spacing1;
+		}
+	}
 }
 </style>
