f2258d3d06248c268cc0d816f527c5972617a2da
jenningschaefer <jenningschaefer@googlemail.com>
2025-08-03 17:14:15 +0200

    new footer design

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,64 +1,101 @@
 <template>
 	<footer>
-		<div
-			id="__by-sailsnake"
-		>
-			<p>build by:</p>
-			<a href="http://www.sailsnake.com" target="_blank" aria-label="link to www.sailsnake.com">
-				<img src="/images/logos/Sailsnake-logo--footer.png" width="941" height="408" alt="Sailsnake creative studio - logo "/>
-			</a
-			>
+		<div id="footer">
+			<div>
+				<NuxtLink to="/">
+					<img src="/images/logos/logo.svg" alt="logo" class="logo" />
+				</NuxtLink>
+			</div>
+			<div>
+				<ul>
+					<li>
+						<NuxtLink to="/">Home</NuxtLink>
+					</li>
+					<li>
+						<NuxtLink to="/">About Me</NuxtLink>
+					</li>
+					<li>
+						<NuxtLink to="/">Blog</NuxtLink>
+					</li>
+					<li>
+						<NuxtLink to="/">Contact</NuxtLink>
+					</li>
+					<li>
+						<NuxtLink to="/">Datenschutz</NuxtLink>
+					</li>
+					<li>
+						<NuxtLink to="/">Impressum</NuxtLink>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<ul>
+					<li><a href=""><img src="/social-media/facebook-f.png" alt="facebook" class="social-icon">
+							biancahambusch</a></li>
+					<li><a href=""><img src="/social-media/instagram.png" alt="instagram" class="social-icon">
+							@biancahambusch</a></li>
+				</ul>
+			</div>
 		</div>
 	</footer>
 </template>
 
 <style lang="scss" scoped>
-	footer {
-		position: relative;
-		height: max-content;
-		display: grid;
-		grid-template-columns: repeat(3, 1fr);
-		gap: $spacing5;
-		margin-inline: $spacing4;
-		opacity: 0;
-		animation: 5s ease 3s forwards appear;
-		@keyframes appear {
-			0% {
-				opacity: 0;
-			}
-			100% {
-				opacity: 1;
-			}
+#footer {
+	background-color: $base-color;
+	position: relative;
+	//height: max-content;
+	display: grid;
+	grid-template-columns: repeat(3, minmax(0, 1fr));
+	gap: $spacing5;
+	align-items: center;
+	justify-content: center;
+	opacity: 0;
+	animation: 5s ease 3s forwards appear;
+
+	@keyframes appear {
+		0% {
+			opacity: 0;
 		}
-		@include media(xsm) {
-			display: block;
-			height: auto;
-			margin-top: $spacing2;
-			margin-inline: 0;
+
+		100% {
+			opacity: 1;
 		}
 	}
 
-	#__by-sailsnake {
-		grid-column: 2;
-		padding: $spacing1;
-		justify-self: center;
-		border-top: 0.1em solid $light-grey;
-		p {
-			margin: 0;
-			display: inline-block;
-			font-family: $font-art;
-		}
-		a {
-			margin-left: $spacing1;
-			display: inline-block;
-			font-style: normal;
-			text-transform: uppercase;
-			font-family: $font-accent;
-			img {
-				height: $font-size5;
-				width: auto;
-				aspect-ratio: 941 / 408;
-			}
+	@include media(xsm) {
+		grid-template-columns: 1fr;
+		gap: $spacing3;
+		padding: $spacing2 0;
+	}
+
+	ul {
+		list-style: none;
+		padding: 0;
+	}
+
+	a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: $white;
+		text-decoration: none;
+
+		&:hover {
+			text-decoration: underline;
 		}
 	}
+}
+
+.logo {
+	width: 100%;
+	max-width: 10em;
+	height: auto;
+}
+
+.social-icon {
+	width: auto;
+	height: 2em;
+	margin: 0 $spacing1;
+}
 </style>
